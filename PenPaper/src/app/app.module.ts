9989fed8c4aa1a06aa5f5caf171b72d8131fb577
Phil <morris.p1990@gmail.com>
2020-01-04 12:02:54 +0000

    add play-area frame
    ng generate component play-area

--- PenPaper/src/app/app.module.ts
+++ PenPaper/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayAreaComponent } from './play-area/play-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayAreaComponent
   ],
   imports: [
     BrowserModule,
