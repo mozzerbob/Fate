c0bf085b361095308b2fb9d3ee3a39ae42303005
Phil <morris.p1990@gmail.com>
2020-01-04 12:55:31 +0000

    add landing page frame

--- PenPaper/src/app/app.module.ts
+++ PenPaper/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayAreaComponent } from './play-area/play-area.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayAreaComponent
+    PlayAreaComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
