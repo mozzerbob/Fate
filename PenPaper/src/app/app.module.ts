9e1a9bbf9828380ee95e792132a9a8614a6f242c
Phil <morris.p1990@gmail.com>
2020-03-10 22:54:56 +0000

    Material play around

--- PenPaper/src/app/app.module.ts
+++ PenPaper/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayAreaComponent } from './play-area/play-area.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [PlayAreaComponent]
--- PenPaper/src/app/play-area/play-area.component.ts
+++ PenPaper/src/app/play-area/play-area.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MaterialModule} from '../material/material.module';
 
 @Component({
   selector: 'app-play-area',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./play-area.component.css']
 })
 export class PlayAreaComponent implements OnInit {
+
+  LandingPage = "Landing Page";
 
   constructor() { }
 
